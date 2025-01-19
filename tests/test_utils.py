c12e38cf4ff77e310fa38bf4be70cefe44866d70
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2025-01-19 23:02:54 +0530

    Write test for get_config

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -61,9 +61,28 @@ class TestLoadConfig:
         is_valid = is_config_valid(config)
         assert is_valid
 
-# class TestGetConfig:
-#     def test_when_load_config_raises_exception(self, mocker):
-#         mocker.patch("resumaker.utils.load_config")
-#         config = get_config()
-#         assert type(config) == dict
-#         assert len(config.keys()) == 1
+@pytest.fixture
+def apply_default_configs():
+    return default_configs
+
+class TestGetConfig:
+    def test_when_not_overwritten(self, apply_default_configs, mocker):
+        mock = mocker.patch("resumaker.utils.get_valid_user_config")
+        mock.return_value = {"key": "value"}
+        config = get_config()
+        assert config == default_configs
+
+    def test_when_overwritten(self, apply_default_configs, mocker):
+        mock = mocker.patch("resumaker.utils.get_valid_user_config")
+        mock.return_value = {"RESUME_YAML_FILENAME": "custom_name.yml"}
+        config = get_config()
+        assert config == {"RESUME_YAML_FILENAME": "custom_name.yml"}
+
+    def test_when_not_supported_config_are_given(self, apply_default_configs, mocker):
+        mock = mocker.patch("resumaker.utils.get_valid_user_config")
+        mock.return_value = {
+            "RESUME_YAML_FILENAME": "custom_name.yml",
+            "MY_CUSTOM_KEY": "MY_CUSTOM_VALUE",
+        }
+        config = get_config()
+        assert config == {"RESUME_YAML_FILENAME": "custom_name.yml"}
