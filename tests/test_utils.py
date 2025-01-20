835bb2dcef08f9725b4d21d2d9d615f5eb37e89d
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2025-01-20 06:45:30 +0530

    Refactor code

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -25,64 +25,81 @@ def create_valid_config_file():
     yield
     os.remove("config.yml")
 
-
-
-class TestLoadConfig:
-    def test_config_file_does_not_exist(self, delete_config_file):
-        config = load_config()
-        assert config == ""
-
-    def test_config_file_does_exist(self, delete_config_file):
-        config = load_config()
-        assert type(config) == str
-
-    def test_get_invalid_user_config(self, mocker):
-        mock = mocker.patch("resumaker.utils.get_rendered_config")
-        mock.return_value = [None]
-
-        with pytest.raises(ValueError) as excinfo:
-            get_valid_user_config()
-        assert excinfo.type is ValueError
-        assert str(excinfo.value) == "config.yml is not valid!"
-
-    def test_get_valid_user_config(self, mocker):
-        mock = mocker.patch("resumaker.utils.get_rendered_config")
-        mock.return_value = {"key": "value"}
-
-        config = get_valid_user_config()
-        assert config == {"key": "value"}
-
-    def test_is_config_valid(self, create_invalid_config_file):
-        config = None
-        is_valid = is_config_valid(config)
-        assert not is_valid
-
-        config = {}
-        is_valid = is_config_valid(config)
-        assert is_valid
-
-@pytest.fixture
-def apply_default_configs():
-    return default_configs
-
 class TestGetConfig:
-    def test_when_not_overwritten(self, apply_default_configs, mocker):
-        mock = mocker.patch("resumaker.utils.get_valid_user_config")
-        mock.return_value = {"key": "value"}
+    def test_when_filenotfound(self, mocker):
+        mocked_load_config = mocker.patch("resumaker.utils.load_config")
+        mocked_load_config.side_effect = FileNotFoundError
         config = get_config()
         assert config == default_configs
 
-    def test_when_overwritten(self, apply_default_configs, mocker):
-        mock = mocker.patch("resumaker.utils.get_valid_user_config")
-        mock.return_value = {"RESUME_YAML_FILENAME": "custom_name.yml"}
-        config = get_config()
-        assert config == {"RESUME_YAML_FILENAME": "custom_name.yml"}
+class TestLoadConfig:
+    def test_config_file_does_not_exist(self, delete_config_file):
+        with pytest.raises(FileNotFoundError):
+            load_config()
 
-    def test_when_not_supported_config_are_given(self, apply_default_configs, mocker):
-        mock = mocker.patch("resumaker.utils.get_valid_user_config")
-        mock.return_value = {
-            "RESUME_YAML_FILENAME": "custom_name.yml",
-            "MY_CUSTOM_KEY": "MY_CUSTOM_VALUE",
+    def test_config_file_does_exist(self, create_valid_config_file):
+        config = load_config()
+        assert type(config) == str
+
+@pytest.fixture
+def apply_user_config_obj(request):
+    marker = request.node.get_closest_marker("config_data")
+    if marker is None:
+        data = None
+    else:
+        data = marker.args[0]
+    return data
+
+class TestMergeUserConfigWithDefaults:
+    @pytest.mark.config_data({})
+    def test_when_not_overwritten(self, apply_user_config_obj, mocker):
+        config = merge_user_config_with_defaults(apply_user_config_obj)
+        assert config == default_configs
+
+    @pytest.mark.config_data({
+        "RESUME_YAML_FILENAME": "custom.yml"
+    })
+    def test_when_overwritten_with_string(self, apply_user_config_obj, mocker):
+        config = merge_user_config_with_defaults(apply_user_config_obj)
+        assert config == {
+            "RESUME_YAML_FILENAME": ["custom.yml"]
         }
-        config = get_config()
-        assert config == {"RESUME_YAML_FILENAME": "custom_name.yml"}
+
+    @pytest.mark.config_data({
+        "RESUME_YAML_FILENAME": ["custom.yml"]
+    })
+    def test_when_overwritten_with_list(self, apply_user_config_obj, mocker):
+        config = merge_user_config_with_defaults(apply_user_config_obj)
+        assert config == {
+            "RESUME_YAML_FILENAME": ["custom.yml"]
+        }
+
+    @pytest.mark.config_data({
+        "custom_config": ["custom_value"]
+    })
+    def test_ignore_unsupported_config(self, apply_user_config_obj, mocker):
+        config = merge_user_config_with_defaults(apply_user_config_obj)
+        assert config == default_configs
+
+class TestValidateConfigTxt:
+    def test_when_dict(self, mocker):
+        mock = mocker.patch("resumaker.utils.yaml_to_pyobj")
+        mock.return_value = {}
+        config = validate_config_txt('')
+        assert config == {}
+
+    def test_when_list(self, mocker):
+        mock = mocker.patch("resumaker.utils.yaml_to_pyobj")
+        mock.return_value = []
+        with pytest.raises(SystemExit) as excinfo:
+            config = validate_config_txt('')
+            assert excinfo.type == SystemExit
+            assert str(excinfo.value) == "config.yml is not valid!"
+
+    def test_when_None(self, mocker):
+        mock = mocker.patch("resumaker.utils.yaml_to_pyobj")
+        mock.return_value = None
+        with pytest.raises(SystemExit) as excinfo:
+            config = validate_config_txt('')
+            assert excinfo.type == SystemExit
+            assert str(excinfo.value) == "config.yml is not valid!"
