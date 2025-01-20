d285b5b7cbeb5efb92b1104222c9be38574e83dc
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2025-01-20 12:19:45 +0530

    Write test for utils

--- src/resumaker/utils.py
+++ src/resumaker/utils.py
@@ -3,8 +3,18 @@ import os
 import yaml
 
 CONFIG_FILENAME = "config.yml"
-
 default_configs = {"RESUME_YAML_FILENAME": ["resume.yml", "resume.yaml"]}
+
+def get_config():
+    """Return config data by overwriting default_configs
+    by reading config.yml
+    """
+    try:
+        user_config_txt = load_config()
+    except FileNotFoundError:
+        return default_configs
+    else:
+        return manage_user_config_txt(user_config_txt)
 
 
 def load_config():
@@ -12,38 +22,27 @@ def load_config():
     If config.yml is valid (a dict), it return it.
     else sys.exit
     """
-    try:
-        with open(CONFIG_FILENAME) as f:
-            return f.read()
-    except FileNotFoundError:
-        return ""
+    with open(CONFIG_FILENAME) as f:
+        return f.read()
 
-def render_yaml(config_txt):
-    return yaml.safe_load(config_txt)
+def manage_user_config_txt(user_config_txt):
+    user_config_obj = validate_config_txt(user_config_txt)
+    return merge_user_config_with_defaults(user_config_obj)
 
-def get_rendered_config():
-    config_txt = load_config()
-    config_obj = render_yaml(config_txt)
-
-def is_config_valid(config):
-    return type(config) == dict
-
-def get_valid_user_config():
-    config = get_rendered_config()
-    if not is_config_valid(config):
-        raise ValueError("config.yml is not valid!")
-    else:
-        return config
-    
-
-def get_config():
-    """Return config data by overwriting default_configs
-    by reading config.yml
-    """
-    user_configs = get_valid_user_config()
-    for config in user_configs:
+def merge_user_config_with_defaults(user_config_obj):
+    for config in user_config_obj:
         if config in default_configs:
-            default_configs[config] = user_configs[config]
+            user_config_value = user_config_obj[config]
+            default_configs[config] = user_config_value if type(user_config_value) == list else [user_config_value]
         else:
             print(f"{config} is not supported!")
     return default_configs
+
+def validate_config_txt(txt):
+    config_obj = yaml_to_pyobj(txt)
+    if type(config_obj) == dict:
+        return config_obj
+    sys.exit("config.yml is not valid!")
+
+def yaml_to_pyobj(yaml_txt):
+    return yaml.safe_load(yaml_txt)
